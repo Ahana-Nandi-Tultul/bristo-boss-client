ead488e70cc9f7502a088ca5b69c663981f1575f
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-08-30 21:08:29 +0600

    category added

---
+++ src/components/sectionTitle/SectionTitle.jsx
@@ -0,0 +1,11 @@
+
+const SectionTitle = ({heading, subHeading}) => {
+    return (
+        <div className="text-center space-y-4 mb-10 md:w-3/12 mx-auto">
+            <p className="text-yellow-500">---{heading}---</p>
+            <h2 className="text-4xl uppercase border-y-2">{subHeading}</h2>
+        </div>
+    );
+};
+
+export default SectionTitle;
