7de59d53402a3bf1f4eefd5f178188f97ef99317
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-09-20 21:14:43 +0600

    complete stripe payment

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,10 +1,26 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
 
-const CheckoutForm = () => {
+const CheckoutForm = ({price}) => {
+    const {user} = useAuth();
     const stripe = useStripe();
     const elements = useElements();
-    const [cardError, setCardError] = useState('')
+    const [cardError, setCardError] = useState('');
+    const [instance] = useAxiosSecure();
+    const [clientSecret, setClientSecret] = useState('');
+    const [processing, setProcessing] = useState(false);
+    const [transactionId, setTransactionId] = useState('');
+
+    useEffect(()=> {
+        instance.post('/create-payment-intent', {price})
+        .then(res => {
+            // console.log(res?.data?.clientSecret)
+            setClientSecret(res?.data?.clientSecret)
+        })
+    }, [])
+
     const handleSubmit = async(event) => {
         event.preventDefault();
         if(!stripe || !elements){
@@ -26,8 +42,30 @@ const CheckoutForm = () => {
             setCardError(error.message);
           } else {
             setCardError('')
-            console.log('[PaymentMethod]', paymentMethod);
+            // console.log('[PaymentMethod]', paymentMethod);
           }
+          setProcessing(true)
+          const {paymentIntent, confirmError} = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+              payment_method: {
+                card: card,
+                billing_details: {
+                  email: user?.email || 'unKnown',
+                  name: user?.displayName || 'anonymous',
+                },
+              },
+            },
+          );
+
+          if(confirmError){
+            console.log(confirmError)
+          }
+          if(paymentIntent.status === "succeeded"){
+            setProcessing(false);
+            setTransactionId(paymentIntent.id);
+          }
+          console.log('payment intent: ',paymentIntent);
         
     }
     return (
@@ -49,11 +87,12 @@ const CheckoutForm = () => {
                     },
                     }}
                 />
-                <button type="submit" disabled={!stripe} className="btn btn-primary btn-sm text-white mt-4">
+                <button type="submit" disabled={!stripe || !clientSecret || processing} className="btn btn-primary btn-sm text-white mt-4">
                     Pay
                 </button>
             </form>
             {cardError && <p className="text-red-600"><small>{cardError}</small></p>}
+            {transactionId && <p className="text-green-600"><small>Transaction complete with transactionId: {transactionId}</small></p>}
         </div>
         
     );
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,16 +2,20 @@ import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/sectionTitle/sectionTitle";
 import CheckoutForm from "./CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
+import useCart from "../../../hooks/useCart";
 
 
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Payment = () => {
+    const [carts] = useCart()
+    const totalPrice = carts.reduce((sum, item) => item.price + sum, 0);
+    const price = parseFloat(totalPrice.toFixed(2))
     return (
         <div className="w-full">
             <SectionTitle heading="Please Procced" subHeading="Payment"></SectionTitle>
             <Elements stripe={stripePromise}>
-                <CheckoutForm></CheckoutForm>
+                <CheckoutForm price={price}></CheckoutForm>
             </Elements>
         </div>
     );
